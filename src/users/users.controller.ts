88cb004e301dc21966086fef8a9d9d3b5de983da
thiagodeas <sousaalvesth@gmail.com>
2025-01-27 11:37:19 -0300

    refatorando users controller e preparando para implementar jwt

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor (private readonly usersService: UsersService){}
+
+    @Get()
+    async getUsers() {
+        return this.usersService.findAll();
+    }
+
+    @Post()
+    async createUser(@Body() createUserDto: CreateUserDTO) {
+        return this.usersService.createUser(createUserDto);
+    }
+
+}

47bbc8b210a93f4befb1d2f2d84576a7e152936e
thiagodeas <sousaalvesth@gmail.com>
2025-01-30 10:40:28 -0300

    implementando método register no auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDTO } from 'src/users/dto/login-user.dto';
+import { CreateUserDTO } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor (private readonly authService: AuthService){}
 
+    @Post('register')
+    async register (@Body() createUserDto: CreateUserDTO) {
+        return this.authService.register(createUserDto);
+    }
+
     @Post('login')
     async login (@Body() loginUserDto: LoginUserDTO) {
         return this.authService.login(loginUserDto);
     }
+    
 }

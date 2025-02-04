c4c433d68d182dd8d991f7c2ceb76898de170412
thiagodeas <sousaalvesth@gmail.com>
2025-02-04 20:20:09 -0300

    adicionando rota para um usuario ter acesso apenas aos seus proprios dados

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,28 @@
-import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { Role } from '@prisma/client';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { AuthRequest } from 'src/auth/auth-request.interface';
 
 @Controller('users')
 export class UsersController {
     constructor (private readonly usersService: UsersService){}
 
-    @Get()
     @UseGuards(AuthGuard('jwt'), RolesGuard)
     @Roles(Role.ADMIN)
+    @Get()
     async findAllUsers () {
         return this.usersService.findAll();
     }
+
+     @UseGuards(AuthGuard('jwt'))
+     @Get('me')
+     async getMyProfile(@Request() req: AuthRequest) {
+ 
+         return this.usersService.findUserById(String(req.user.id));
+     }
 
     @UseGuards(AuthGuard('jwt'), RolesGuard)
     @Roles(Role.ADMIN)
@@ -29,4 +37,6 @@ export class UsersController {
     async deleteUserById(@Param('id') id: string) {
         return this.usersService.deleteUser(id);
     }
+
+   
 }

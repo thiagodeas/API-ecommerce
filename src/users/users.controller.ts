0852453087ebefc4d49cf097db5d4741a6d7e1df
thiagodeas <sousaalvesth@gmail.com>
2025-02-04 10:20:49 -0300

    restringindo rotas de gerenciamento de usuarios apenas para admin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,32 @@
 import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from '@prisma/client';
 import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('users')
 export class UsersController {
     constructor (private readonly usersService: UsersService){}
 
     @Get()
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(Role.ADMIN)
     async findAllUsers () {
         return this.usersService.findAll();
     }
 
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(Role.ADMIN)
     @Get(':id')
     async findUserById(@Param('id') id: string) {
         return this.usersService.findUserById(id);
     }
 
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(Role.ADMIN)
     @Delete(':id')
     async deleteUserById(@Param('id') id: string) {
         return this.usersService.deleteUser(id);
     }
-}npx prisma generate
-
+}

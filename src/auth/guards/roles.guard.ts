e0ecd21cb337c66af9b1f91f65596d86ff228c6d
thiagodeas <sousaalvesth@gmail.com>
2025-02-04 10:08:56 -0300

    criando decorator e guard de roles

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Role } from "@prisma/client";
+import { Request } from "express";
+import { AuthenticatedUser } from "../authenticated-user.interface";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor (private reflector: Reflector) {}
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (!requiredRoles) {
+            return true;
+        }
+
+        const request = context.switchToHttp().getRequest<Request>();
+        const user = request.user as AuthenticatedUser;
+
+        if (!user || !requiredRoles.includes(user.role)) {
+            throw new ForbiddenException('Acesso negado! Você não tem permissão para acessar essa rota.');
+        }
+
+        return true;
+    }
+}

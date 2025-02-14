df938c90086c0fc0bedd72cefe01eaf6bd682a85
thiagodeas <sousaalvesth@gmail.com>
2025-02-13 22:40:43 -0300

    refatorando dto de resposta do carrinho de compras

--- src/cart/dto/cart-response.dto.ts
+++ src/cart/dto/cart-response.dto.ts
@@ -1,11 +1,19 @@
-import { Product } from "@prisma/client";
+import { CartItem, Product } from "@prisma/client";
 
 export class CartItemResponseDTO {
     productId: number;
+    productName: string;
+    price: number;
     quantity: number;
     subtotal: number;
 
-    product: Product;
+    constructor(cartItem: CartItem & { product: Product }) {
+        this.productId = cartItem.product.id;
+        this.productName = cartItem.product.name;
+        this.price = cartItem.product.price;
+        this.quantity = cartItem.quantity;
+        this.subtotal = Number((cartItem.quantity * cartItem.product.price).toFixed(2));
+    }
 }
 
 export class CartResponseDTO {
@@ -13,4 +21,11 @@ export class CartResponseDTO {
     userId: number;
     items: CartItemResponseDTO[];
     total: number;
+
+    constructor(cart: { id: number; userId: number; items: (CartItem & { product: Product })[] }) {
+        this.id = cart.id;
+        this.userId = cart.userId;
+        this.items = cart.items.map((item) => new CartItemResponseDTO(item));
+        this.total = Number(this.items.reduce((sum, item) => sum + item.subtotal, 0).toFixed(2));
+    }
 }

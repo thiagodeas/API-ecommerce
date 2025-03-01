b4d9bdf7b112375f5301fa9be77ae2e6814c24a8
thiagodeas <sousaalvesth@gmail.com>
2025-03-01 09:47:48 -0300

    chore: alterando a porta do servidor

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,12 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   const config = new DocumentBuilder()
   .setTitle('E-commerce API')
   .setDescription('API de e-commerce que gerencia produtos, carrinho e usuarios')
@@ -19,6 +25,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();

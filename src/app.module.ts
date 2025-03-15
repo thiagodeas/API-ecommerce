6a157331ef192866348fc80070f45c1afc5cdff1
thiagodeas <sousaalvesth@gmail.com>
2025-03-15 12:23:39 -0300

    feat: adicionando suporte a variável de ambiente para conexão com o MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,16 @@ import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CartModule } from './cart/cart.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/ecommerce-db'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || (() => { throw new Error('MONGODB_URI não está definida nas variáveis de ambiente.');
+   })()
+   ),
     UsersModule,
     AuthModule,
     ProductsModule,

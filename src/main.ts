833ce935076a6083cf028259c3194c14dcb030b6
thiagodeas <sousaalvesth@gmail.com>
2025-03-17 10:55:21 -0300

    feat: dockeirizando a aplicação

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
